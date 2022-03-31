3e85f770b569abfe34aa0358e7a76e6d011bfe8c
Bhavik Solanki <bhavik.solanki@enterprisewide.com>
2022-03-31 23:36:43 +0530

    Added Layout examples

---
+++ AnchorPane/src/module-info.java
@@ -0,0 +1,6 @@
+module AnchorPane {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ JavaFxControls/src/module-info.java
@@ -0,0 +1,6 @@
+module JavaFxControls {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
